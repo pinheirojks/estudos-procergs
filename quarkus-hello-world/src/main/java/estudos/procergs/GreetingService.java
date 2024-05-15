d590456b9ab95e1fbd9729fe43f3c9b4cdbdd07c
Heitor Oliveira <heitorcostaol@gmail.com>
2024-05-15 11:27:24 -0300

    primeira aplicação quarkus

---
+++ quarkus-hello-world/src/main/java/estudos/procergs/GreetingService.java
@@ -0,0 +1,12 @@
+package estudos.procergs;
+
+import jakarta.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class GreetingService {
+
+    public String hello(){
+        return "Olá mundo from service";
+    }
+    
+}
