93cfd1c418710ff1461f0efa59daf77a283118d3
jackson-pinheiro <jackson-pinheiro@procergs.rs.gov.br>
2024-05-16 14:12:13 -0300

    Adição do swagger

--- quarkus-hello-world/src/main/java/estudos/procergs/resource/UsuarioResource.java
+++ quarkus-hello-world/src/main/java/estudos/procergs/resource/UsuarioResource.java
@@ -2,6 +2,9 @@ package estudos.procergs.resource;
 
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import estudos.procergs.entity.Usuario;
 import estudos.procergs.usuario.UsuarioService;
 import jakarta.inject.Inject;
@@ -14,12 +17,14 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/usuario")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Usuários", description = "CRUD de usuários")
 public class UsuarioResource {
 
     @Inject
     private UsuarioService usuarioService;
 
     @GET
+    @Operation(description = "Lista todos os usuários")
     public List<Usuario> listaTodos(){
         return usuarioService.listaTodos();
     }
