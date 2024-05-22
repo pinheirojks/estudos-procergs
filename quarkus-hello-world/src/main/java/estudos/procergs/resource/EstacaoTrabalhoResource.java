0b6d8c5ca61f9b074dbdeefe24db28d89a20ec54
Antonio Souza <antonio-azevedo@procergs.rs.gov.br>
2024-05-22 18:29:50 -0300

    remocao do interceptor do rest de estacoes

--- quarkus-hello-world/src/main/java/estudos/procergs/resource/EstacaoTrabalhoResource.java
+++ quarkus-hello-world/src/main/java/estudos/procergs/resource/EstacaoTrabalhoResource.java
@@ -1,13 +1,8 @@
 package estudos.procergs.resource;
 
-import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
-import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
-import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
-import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import estudos.procergs.entity.EstacaoTrabalho;
-import estudos.procergs.infra.AutorizacaoRest;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
@@ -18,9 +13,6 @@ import jakarta.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Tag(name = "Estação Trabalho", description = "CRUD de estação de trabalho")
-@AutorizacaoRest
-@SecuritySchemes(value = { @SecurityScheme(securitySchemeName = "apiKey",  type = SecuritySchemeType.HTTP, scheme = "Bearer") })
-@SecurityRequirement(name = "apiKey")
 public interface EstacaoTrabalhoResource extends PanacheEntityResource<EstacaoTrabalho,Long>{
     
 }
